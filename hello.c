f63fbd1dee1a9a5a6760ba8ee33304abfb5faa48
seanmiller81 <46793792+seanmiller81@users.noreply.github.com>
2019-01-17 13:03:08 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Sean
+ * Date: 2019/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sean\n");
+
+  printf("Electrical Engineering\n")
+
+  return 0;
+}
